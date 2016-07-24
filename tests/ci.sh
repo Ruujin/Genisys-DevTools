1a28c85422be0af39e35d5dd0d8039136a9291dd
dktapps <odigiman@gmail.com>
2016-07-24 16:58:55 +0100

    Add Travis CI configuration and CI script

---
+++ tests/ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo Running lint...
+shopt -s globstar
+for file in **/*.php; do
+    OUTPUT=`php -l "$file"`
+    [ $? -ne 0 ] && echo -n "$OUTPUT" && exit 1
+done
+echo Lint done successfully.
+mkdir build
+php src/DevTools/DevTools.php --make src --out ../build/Genisys-DevTools.phar
+if ls build/Genisys-DevTools*.phar >/dev/null 2>&1; then
+    echo Server packaged successfully.
+else
+    echo No phar created!
+    exit 1
+fi
